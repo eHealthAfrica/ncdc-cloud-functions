5f7b4c6971d57ce404ece615559f871a77e8f211
lordmallam <lordy002000@gmail.com>
2020-05-21 17:12:50 +0100

    fix: substracter

--- javascript/simple_calc/index.js
+++ javascript/simple_calc/index.js
@@ -1,5 +1,5 @@
 exports.subtracter = (req, res) => {
     const x = parseInt(req.query.x)
     const y = parseInt(req.query.y)
-    return res.send(`${x + y}`);
+    return res.send(`${x - y}`);
 };
