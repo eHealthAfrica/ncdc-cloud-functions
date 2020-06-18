70c545d449f9be3bcdd0e6a77aa95826b91f2665
lordmallam <lordy002000@gmail.com>
2020-06-18 10:07:13 +0100

    update functions

---
+++ python/ncdc_nrl/helpers/utils.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import json
+
+
+def read_xlxs(request):
+    content_type = request.headers.get('Content-Type')
+    data_list = []
+    if content_type.startswith('multipart/form-data'):
+        if 'file' not in request.files:
+            raise ValueError('No file provided.')
+        excel_file = pd.ExcelFile(request.files['file'])
+        sheet_names = excel_file.sheet_names
+        for sheet_name in sheet_names:
+            df = excel_file.parse(sheet_name)
+            sheet_dict = {
+                'name': sheet_name,
+            }
+            data = {}
+            has_started = False
+            for _, row in df.iterrows():
+                row_list = row.values.tolist()
+                if has_started:
+                    data[row_list[1]] = None \
+                        if not isinstance(row_list[3], str) \
+                        and np.isnan(row_list[3]) else row_list[3]
+                else:
+                    has_started = row_list[0] == '#'
+            sheet_dict['data'] = data
+            data_list.append(sheet_dict)
+    return data_list
+
+
+def request_to_ckan_query(request):
+    request_data = json.loads(request.data)
+    categories = request_data.get('categories', [])
+    fields = request_data.get('fields', [])
+    ckan_query_url = \
+        f'/q?categories={json.dumps(categories)}&facets={json.dumps(fields)}'
+    return ckan_query_url
