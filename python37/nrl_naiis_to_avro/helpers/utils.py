39c8fd261002c1561fae6c7ede430640e224c078
lordmallam <lordy002000@gmail.com>
2020-06-19 12:23:18 +0100

    seperate functions

--- python37/nrl_naiis_to_avro/helpers/utils.py
+++ python37/nrl_naiis_to_avro/helpers/utils.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import json
 
 
 def read_xlxs(request):
@@ -29,12 +28,3 @@ def read_xlxs(request):
             sheet_dict['data'] = data
             data_list.append(sheet_dict)
     return data_list
-
-
-def request_to_ckan_query(request):
-    request_data = json.loads(request.data)
-    categories = request_data.get('categories', [])
-    fields = request_data.get('fields', [])
-    ckan_query_url = \
-        f'/q?categories={json.dumps(categories)}&facets={json.dumps(fields)}'
-    return ckan_query_url
--- python37/nrl_naiis_to_avro/main.py
+++ python37/nrl_naiis_to_avro/main.py
@@ -1,17 +1,10 @@
 import flask
 import json
-# import pandas as pd
 from helpers.utils import (
     read_xlxs,
-    request_to_ckan_query,
 )
 
 
 def naiis_dict_avro(request):
     data = read_xlxs(request)
-    return flask.make_response(json.dumps(data))
-
-
-def ckan_query_generator(request):
-    data = request_to_ckan_query(request)
     return flask.make_response(json.dumps(data))
