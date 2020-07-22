27578ce8cf46e57e4e9ba38c5594d91bd692306b
lordmallam <lordy002000@gmail.com>
2020-07-22 10:33:52 +0100

    clean ApproveOrReject function

--- python37/ApproveOrReject/main.py
+++ python37/ApproveOrReject/main.py
@@ -1,7 +1,6 @@
 
 import json
-import flask
-from flask import flash, request
+from flask import request
 import requests
 from flask import Response
 from requests.auth import HTTPBasicAuth
@@ -10,38 +9,37 @@ from requests.auth import HTTPBasicAuth
 with open('approve_endpoint.json') as json_file:
 
     data = json.load(json_file)
- 
-# URL = data[0]["url"]
-# Username = data[0]["Username"]
-# Password = data[0]["Password"]
-# print(URL)
-# print(Username)
-# print(Password)
 
-def ApproveOrReject(request):
 
-	# decision=request.args.get('decision')
-	correlationKey1=request.args.get('correlationKey')
-	correlationKey=correlationKey1[:-1]
-	if correlationKey1[-1] == "t":
-		decision = "true"
-	else:
-		decision = "false"
-	# print(decision)
-	# print(correlationKey)
-	PARAMS = {"listener_name":data[0]["listener_name"],"correlationKey": correlationKey,  "variables": { "decision" : decision}}
-	# print(PARAMS)
-	headers = {
-   'Content-Type': "application/json"   
-   }
-	response = requests.request("GET", data[0]["url"],auth=HTTPBasicAuth(data[0]["Username"], data[0]["Password"]), data=json.dumps(PARAMS), headers=headers, params={"id":"default"})
-	
-	print(response)
-	result1 = response.text
-	result = {"success":result1.replace("\n","")}
-	python2json = json.dumps(result)
-	Response(python2json, mimetype='application/json')
-	# print(python2json)
-	# print(type(python2json))
-	return python2json
-	
+def ApproveOrReject():
+    correlationKey1 = request.args.get('correlationKey')
+    correlationKey = correlationKey1[:-1]
+    if correlationKey1[-1] == "t":
+        decision = "true"
+    else:
+        decision = "false"
+    PARAMS = {
+        "listener_name": data[0]["listener_name"],
+        "correlationKey": correlationKey,
+        "variables": {"decision": decision}
+    }
+    headers = {
+        'Content-Type': "application/json"
+    }
+    response = requests.request(
+        "GET",
+        data[0]["url"],
+        auth=HTTPBasicAuth(
+            data[0]["Username"],
+            data[0]["Password"]
+        ),
+        data=json.dumps(PARAMS),
+        headers=headers,
+        params={"id": "default"}
+    )
+
+    result1 = response.text
+    result = {"success": result1.replace("\n", "")}
+    python2json = json.dumps(result)
+    Response(python2json, mimetype='application/json')
+    return python2json
